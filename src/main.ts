4e5140d0d1121ddba364d65d6350aa9e0173dd57
Martin Fort <martinfort13@gmail.com>
2026-06-04 14:47:18 -0300

    Fix: change origin url to enable CORS when deployed

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   ));
   
   const origin = process.env.NODE_ENV === "production" 
-    ? process.env.BASE_URL ?? "https://egg-track.vercel.app"
+    ? process.env.BASE_URL ?? "https://eggtrack.vercel.app"
     : "http://localhost:3000";
   
   app.enableCors({
